8257785981a688b24268d8231bc4081acb585d3c
benmor20 <benmor2020@gmail.com>
2024-03-02 18:15:33 -0500

    First draft of main run script

---
+++ cv_pong/main.py
@@ -0,0 +1,34 @@
+"""
+Main run script for Pong
+"""
+import pygame
+from pygame import locals
+from src.constants import *
+from src.model import PongModel
+from src.view import PygameView
+from src.controller import KeyboardController
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+
+    model = PongModel()
+    view = PygameView(model, screen)
+    controller = KeyboardController(model)
+
+    clock = pygame.time.Clock()
+    exited = False
+    while not exited:
+        for _ in pygame.event.get(locals.QUIT):
+            exited = True
+
+        controller.move()
+        model.update()
+        view.draw()
+
+        clock.tick(FRAME_RATE)
+
+
+if __name__ == '__main__':
+    main()
